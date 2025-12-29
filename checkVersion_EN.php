25375b83a028bcc151ef43272db4588e3257b300
Sean Jou <present810209@gmail.com>
2025-12-29 14:34:03 +0800

    測試 json檔案是否可以執行在靜態網頁上

--- checkVersion_EN.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header("Content-Type:text/html; charest=utf-8");
-$response = "{\"sysCode\": " . "\"" . "200" . "\"" .
-    ",\"sysMsg\":" . "\"" . "" . "\"" .
-    ",\"data\":" . "{\"version\":\"1.5.0\",\"isForceUpdate\":true,\"androidURL\":\"https://play.google.com/store/apps/details?id=com.shaen.China\",\"isError\":false,\"isErrorString\":\"臨時系統維護中\",\"iosURL\":\"https://apps.apple.com/us/app/%E6%B3%A8%E9%9F%B3%E5%AD%B8%E8%8B%B1%E6%96%87/id6499569604\"}" . "}";
-echo $response;
